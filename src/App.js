e0bab96549666c65c1405627f81291a1b1005be9
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-19 10:33:43 -0300

    Criado logica para exibir listagem de projetos do estado.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
@@ -22,13 +23,17 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {projects.map(project => 
+          <li key={project.id}>
+            {project.title}
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
+            </button>
+          </li>
+            
+        )}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
